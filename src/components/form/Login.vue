810d61781437bd55a038e337a455fd88c36a1668
gnobu <inyangubong17@gmail.com>
2023-09-18 12:23:13 +0100

    login/signup components

---
+++ src/components/form/Login.vue
@@ -0,0 +1,16 @@
+<template>
+    <input type="text" data-testId="email" class="input-title" v-model="email" placeholder="Email">
+    <input type="text" data-testId="password" class="input-title" v-model="password" placeholder="Password">
+    <!-- <span class="error-msg">{{ error }}</span> -->
+    <button @click="$emit('submitForm', { email, password })" data-testId="submit-btn" type="submit" class="form-btn">
+        <span>Login</span>
+    </button>
+</template>
+
+<script setup >
+import { ref } from 'vue'
+const emit = defineEmits(['submitForm'])
+
+const email = ref('')
+const password = ref('')
+</script>
---
+++ src/components/form/Signup.vue
@@ -0,0 +1,19 @@
+<template>
+    <input type="text" data-testId="fullName" class="input-title" v-model="fullName" placeholder="Full Name">
+    <input type="text" data-testId="email" class="input-title" v-model="email" placeholder="Email">
+    <input type="text" data-testId="password" class="input-title" v-model="password" placeholder="Password">
+    <!-- <span class="error-msg">{{ error }}</span> -->
+    <button @click="$emit('submitForm', { fullName, email, password })" data-testId="submit-btn" type="submit"
+        class="form-btn">
+        <span>Sign up</span>
+    </button>
+</template>
+
+<script setup >
+import { ref } from 'vue'
+const emit = defineEmits(['submitForm'])
+
+const fullName = ref('')
+const email = ref('')
+const password = ref('')
+</script>
